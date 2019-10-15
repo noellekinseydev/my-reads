010e08338e4edc0f985155196eb482fcb9de92e4
Noelle Horton <49262786+noellekinseydev@users.noreply.github.com>
2019-10-14 20:17:48 -0700

    ...

---
+++ build/precache-manifest.e58c650b6f0b81ddaf61bc7700046e17.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0986d340b2c77afaff1aa4fd95e39295",
+    "url": "/my-reads/index.html"
+  },
+  {
+    "revision": "e6f26b62799d775a54ac",
+    "url": "/my-reads/static/css/main.a6fc746b.chunk.css"
+  },
+  {
+    "revision": "27e1b0fc5922c4dd797f",
+    "url": "/my-reads/static/js/2.fd0a9094.chunk.js"
+  },
+  {
+    "revision": "e6f26b62799d775a54ac",
+    "url": "/my-reads/static/js/main.912252ea.chunk.js"
+  },
+  {
+    "revision": "9ad26cbc0e6b1253fcb2",
+    "url": "/my-reads/static/js/runtime-main.300a46a8.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/my-reads/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/my-reads/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/my-reads/static/media/arrow-drop-down.9d4e3f36.svg"
+  }
+]);
